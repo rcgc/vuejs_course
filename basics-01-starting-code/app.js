10f6d8f5df31082d1e2d9a8470322265ae261eb3
rcgc <a01702388@itesm.mx>
2022-03-26 13:57:05 -0600

    first tutorial progess added

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -2,9 +2,20 @@ const app = Vue.createApp({
     data() {
       return {
         counter: 0,
+        name: '',
+        confirmedName: ''
       };
     },
     methods: {
+      confirmedInput(){
+        this.confirmedName = this.name;
+      },
+      submitForm(){
+        alert('Submitted!');
+      },
+      setName(event){
+        this.name = event.target.value; 
+      },
         add(n){
            this.counter+=n; 
         },
